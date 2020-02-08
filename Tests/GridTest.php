ead4f486926a412d674e681af7553142a6e4da14
phptheme <dev@getphptheme.com>
2020-02-08 20:02:26 +0300

    n/a

--- Tests/GridTest.php
+++ Tests/GridTest.php
@@ -25,13 +25,19 @@ class GridTest extends \PHPUnit\Framework\TestCase
                 'header_1',
                 ['content' => 'header_2'],
                 new GridHeader(['content' => 'header_3'], new Grid)
-            ]
+            ],
+            'items' => function() {
+                yield ['col_1_1', 'col_1_2', 'col_1_3'];
+                yield ['col_2_1', 'col_2_2', 'col_2_3'];
+            }
         ]);
 
         $content = $grid->toString();
 
         $this->assertEquals($content, '<table class="class_1 class_2">'
                 . '<tr><th>header_1</th><th>header_2</th><th>header_3</th></tr>'
+                . '<tr><td>col_1_1</td><td>col_1_2</td><td>col_1_3</td></tr>'
+                . '<tr><td>col_2_1</td><td>col_2_2</td><td>col_2_3</td></tr>'
             .'</table>');
     }
 
